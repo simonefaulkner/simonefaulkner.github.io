8fa32548d5799e8f2c61f828adc338a7cd92ae43
simonefaulkner <55116780+simonefaulkner@users.noreply.github.com>
2019-10-05 12:01:22 -0700

    Add files via upload

--- ratings.js
+++ ratings.js
@@ -1,6 +1,15 @@
-var fives=0;
+
+var ratings= [0,0,0,0,0]
+var total=0
+var totalratings=0
 function addrating(rating){
-	fives=fives+1;
-	var number =document.getElementById("fives");
-	number.innerHTML=fives;
+	var location=rating-1
+	ratings [location]=ratings [location]+1;
+	var number =document.getElementById(rating.toString());
+	number.innerHTML=ratings[location];
+	totalratings=totalratings+1;
+	total=total+rating;
+	var average=total/totalratings;
+	var averageratings=document.getElementById("averageratings");
+	averageratings.innerhtml=average.toString()+" average out of "+total.toString()+ "ratings"
 }
